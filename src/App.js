36c3b6e9ca1e2ad51754799946dd30b42673fa8d
rajan <rajyotiroshan@gmail.com>
2023-04-20 21:39:33 +0530

    outline and variations

--- src/App.js
+++ src/App.js
@@ -4,20 +4,81 @@ export default function App() {
   return (
     <div>
       <div>
-        <Button primary>Primay</Button>
+        <div>
+          <Button primary>Primay</Button>
+        </div>
+        <div>
+          <Button secondary>Secondary</Button>
+        </div>
+        <div>
+          <Button warning>Warning</Button>
+        </div>
+        <div>
+          <Button danger>Danger</Button>
+        </div>
+
+        <div>
+          <Button success>Success</Button>
+        </div>
       </div>
+      {/**Rounded */}
       <div>
-        <Button secondary>Secondary</Button>
-      </div>
-      <div>
-        <Button warning>Warning</Button>
-      </div>
-      <div>
-        <Button danger>Danger</Button>
+        <div>
+          <Button primary rounded>
+            Primay
+          </Button>
+        </div>
+        <div>
+          <Button secondary rounded>
+            Secondary
+          </Button>
+        </div>
+        <div>
+          <Button warning rounded>
+            Warning
+          </Button>
+        </div>
+        <div>
+          <Button danger rounded>
+            Danger
+          </Button>
+        </div>
+
+        <div>
+          <Button success rounded>
+            Success
+          </Button>
+        </div>
       </div>
 
+      {/**Rounded */}
       <div>
-        <Button success>Success</Button>
+        <div>
+          <Button primary outline>
+            Primay
+          </Button>
+        </div>
+        <div>
+          <Button secondary outline>
+            Secondary
+          </Button>
+        </div>
+        <div>
+          <Button warning outline>
+            Warning
+          </Button>
+        </div>
+        <div>
+          <Button danger outline>
+            Danger
+          </Button>
+        </div>
+
+        <div>
+          <Button success outline>
+            Success
+          </Button>
+        </div>
       </div>
     </div>
   );
