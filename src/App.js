18ad0543145e9c60dff89c3d397de251b887c223
rajan <rajyotiroshan@gmail.com>
2023-04-29 18:39:31 +0530

    Accordion init

--- src/App.js
+++ src/App.js
@@ -1,101 +1,26 @@
-import Button from "./components/Button";
-import { GoBell, GoCloudDownload, GoDatabase } from "react-icons/go";
+import Accordion from "./components/Accordion";
 export default function App() {
-  const handleClick = () => {
-    console.log("Click !!!");
-  };
-  const mouseOver = () => {
-    console.log("Mouse Over !!!");
-  };
-  return (
-    <div className="btns">
-      <div className="btnc-container">
-        <div>
-          <Button
-            className=""
-            primary
-            onClick={handleClick}
-            onMouseOver={mouseOver}
-          >
-            <GoBell /> Primay
-          </Button>
-        </div>
-        <div>
-          <Button secondary>
-            <GoCloudDownload />
-            Secondary
-          </Button>
-        </div>
-        <div>
-          <Button warning>Warning</Button>
-        </div>
-        <div>
-          <Button danger>Danger</Button>
-        </div>
-
-        <div>
-          <Button success>Success</Button>
-        </div>
-      </div>
-      {/**Rounded */}
-      <div className="btnc-container">
-        <div>
-          <Button primary rounded>
-            Primay
-          </Button>
-        </div>
-        <div>
-          <Button secondary rounded>
-            Secondary
-          </Button>
-        </div>
-        <div>
-          <Button warning rounded>
-            Warning
-          </Button>
-        </div>
-        <div>
-          <Button danger rounded>
-            Danger
-          </Button>
-        </div>
-
-        <div>
-          <Button success rounded>
-            Success
-          </Button>
-        </div>
-      </div>
-
-      {/**Rounded */}
-      <div className="btnc-container">
-        <div>
-          <Button primary>
-            <GoDatabase /> Primay
-          </Button>
-        </div>
-        <div>
-          <Button secondary outline>
-            Secondary
-          </Button>
-        </div>
-        <div>
-          <Button warning outline>
-            Warning
-          </Button>
-        </div>
-        <div>
-          <Button danger outline>
-            Danger
-          </Button>
-        </div>
-
-        <div>
-          <Button success outline>
-            Success
-          </Button>
-        </div>
-      </div>
-    </div>
-  );
+  const items = [
+    {
+      label: "can i use react?",
+      id: "sdsd3242",
+      content: "Use in any web apps!",
+    },
+    {
+      id: "srr3242",
+      label: "can i use js?",
+      content: "Use in any web apps!",
+    },
+    {
+      id: "sdsdre42",
+      label: "can i use css?",
+      content: "Use in any web apps!",
+    },
+    {
+      id: "sds24242",
+      label: "can i use Vue.js?",
+      content: "Use in any web apps!",
+    },
+  ];
+  return <Accordion items={items} />;
 }
