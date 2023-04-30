bd51093ad5ed07e13b749b781704ede526f93335
rajan <rajyotiroshan@gmail.com>
2023-04-30 13:40:57 +0530

    evt handler

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -2,12 +2,15 @@ import { useState } from "react";
 
 export default function Accordion({ items }) {
   const [expandedIndex, setExpandedIndex] = useState(1);
+  const handleClick = (nxtIndex) => {
+    setExpandedIndex(nxtIndex);
+  };
   const renderedItems = items.map((item, index) => {
     const isExpanded = index === expandedIndex;
 
     return (
       <div key={item.id}>
-        <div>{item.label}</div>
+        <div onClick={() => handleClick(index)}>{item.label}</div>
         {isExpanded && <div>{item.content}</div>}
       </div>
     );
