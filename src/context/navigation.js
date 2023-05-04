b91ebff06072be499726651c272ec06d7e9a7fbb
rajan <rajyotiroshan@gmail.com>
2023-05-04 19:29:03 +0530

    currPath

--- src/context/navigation.js
+++ src/context/navigation.js
@@ -1,10 +1,23 @@
-import { createContext } from "react";
+import { createContext, useState, useEffect } from "react";
 
 const NavigationContext = createContext();
 
 function NavigationProvider({ children }) {
+  const [currPath, setCurrPath] = useState(window.location.pathname);
+  useEffect(() => {
+    const handler = (evt) => {
+        setCurrPath(window.location.pathname)
+    };
+
+    window.addEventListener("popstate", handler);
+
+    return () => {
+      window.removeEventListener("popstate", handler);
+    };
+  }, []);
   return (
     <NavigationContext.Provider value={{}}>
+      {currPath}
       {children}
     </NavigationContext.Provider>
   );
