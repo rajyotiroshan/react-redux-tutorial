43f446329ac948a445655d77c0ead96b6e1b55ef
rajan <rajyotiroshan@gmail.com>
2023-05-09 17:53:52 +0530

    actionbar

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom";
 
-function Modal({ onClose }) {
+function Modal({ onClose, actionBar, children }) {
   return ReactDOM.createPortal(
     <div>
       <div
@@ -8,7 +8,10 @@ function Modal({ onClose }) {
         className="absolute inset-0 bg-gray-300 opacity-80"
       ></div>
       {/** center window with bg white */}
-      <div className="absolute inset-40 p-10 bg-white">I am a Modal</div>
+      <div className="absolute inset-40 p-10 bg-white">
+        {children}
+        {actionBar}
+      </div>
     </div>,
     document.querySelector(".modal-container")
   );
--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -3,21 +3,32 @@ import Button from "../components/Button";
 import { useState } from "react";
 export default function ModalPage() {
   const [showModal, setShowModal] = useState(false);
-
-  const handleClick = () => {
-    setShowModal(!showModal);
-  };
-
   const handleClose = () => {
     setShowModal(false);
   };
+  const handleClick = () => {
+    setShowModal(!showModal);
+  };
+  const actionBar = (
+    <div>
+      <Button primary onClick={handleClose}>
+        I accept
+      </Button>
+    </div>
+  );
+
+  const modal = (
+    <Modal onClose={handleClose} actionBar={actionBar}>
+      <p>Here is an important agreement you need to accept</p>
+    </Modal>
+  );
 
   return (
     <div className="relative">
       <Button primary onClick={handleClick}>
         Open Modal
       </Button>
-      {showModal && <Modal onClose={handleClose} />}
+      {showModal && modal}
     </div>
   );
 }
