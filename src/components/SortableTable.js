fd097df97d7ea58424a43af5e9b60a8e5cb66f84
rajan <rajyotiroshan@gmail.com>
2023-05-11 19:25:52 +0530

    sorting logic v1

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -1,5 +1,60 @@
+import { useState } from "react";
 import Table from "./Table";
+
 function SortableTable(props) {
-  return <Table {...props} />;
+  const [sortOrder, setSortOrder] = useState(null);
+  const [sortBy, setSortBy] = useState(null);
+
+  const { config, data } = props;
+
+  const handleClick = (label) => {
+    if (sortOrder === null) {
+      setSortOrder("asc");
+      setSortBy(label);
+    } else if (sortOrder === "asc") {
+      setSortOrder("desc");
+      setSortBy(label);
+    } else if (sortOrder === "desc") {
+      setSortOrder("null");
+      setSortBy(null);
+    }
+  };
+  //not modifying config, map=> create
+  const updatedConfig = config.map((col) => {
+    if (!col.sortValue) {
+      return col;
+    }
+    return {
+      ...col,
+      header: () => (
+        <th onClick={() => handleClick(col.label)}>{col.label} IS SORTABLE</th>
+      ),
+    };
+  });
+
+  //only sort data if sortOrder and sortBy are not null
+  //Make a copy of the 'data' prop.
+  //Find the correct sort value function AND ISE IT FOR SORTING
+  let sortedData = data;
+  if (sortOrder && sortBy) {
+    const { sortValue } = config.find((column) => column.label === sortBy);
+    sortedData = [...data].sort((a, b) => {
+      const valA = sortValue(a);
+      const valB = sortValue(b);
+
+      const reverseOrder = sortOrder === "asc" ? 1 : -1;
+      if (typeof valA === "string") {
+        return valA.localeCompare(valB) * reverseOrder;
+      } else {
+        return (valA - valB) * reverseOrder;
+      }
+    });
+  }
+  return (
+    <div>
+      {sortBy} - {sortOrder}
+      <Table {...props} data={sortedData} config={updatedConfig} />
+    </div>
+  );
 }
 export default SortableTable;
--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,4 +1,4 @@
-import SortableTable from "../components/Table";
+import SortableTable from "../components/SortableTable";
 function TablePage() {
   const data = [
     { name: "Orange", color: "bg-orange-500", score: 5 },
@@ -14,14 +14,13 @@ function TablePage() {
       sortValue: (fruit) => fruit.name,
     },
     {
-      label: "Color",
+      label: "Colour",
       render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`}></div>,
     },
     {
       label: "Score",
       render: (fruit) => fruit.score,
       sortValue: (fruit) => fruit.score,
-      header: () => <th className="bg-red-500">Score</th>,
     },
   ];
 
