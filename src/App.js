e0b7715c7ff6c1b4d85a1a4396a3b4c68e1e2266
rajan <rajyotiroshan@gmail.com>
2023-05-12 14:20:49 +0530

    CounterPage

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import ButtonPage from "./pages/ButtonPage";
 import Sidebar from "./components/Sidebar";
 import ModalPage from "./pages/ModalPage";
 import TablePage from "./pages/TablePage";
+
+import CounterPage from "./pages/CounterPage";
 export default function App() {
   return (
     <div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
@@ -29,6 +31,9 @@ export default function App() {
 
         <Route path="/table">
           <TablePage />
+        </Route>
+        <Route path="/counter">
+          <CounterPage initialCount={10} />
         </Route>
       </div>
     </div>
