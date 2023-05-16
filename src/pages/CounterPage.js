9c663e0fcb4852f1945f2c2b5d682244d36eb3db
rajan <rajyotiroshan@gmail.com>
2023-05-16 17:04:44 +0530

    incremernt, decrmetn btn

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,13 +1,21 @@
-import useCounter from "../hooks/use-counter";
+import { useState } from "react";
 import Button from "../components/Button";
 
 export default function CounterPage({ initialCount }) {
-  const { count, increment } = useCounter(initialCount);
+  const [count, setCount] = useState(initialCount);
 
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
   return (
     <div>
       <h1>Count is {count}</h1>
       <Button onClick={increment}>Increment</Button>
+      <Button onClick={decrement}>Deccrement</Button>
     </div>
   );
 }
