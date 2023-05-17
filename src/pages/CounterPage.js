d5fc190135217ee08e031fbf56cd5e9db51470e7
rajan <rajyotiroshan@gmail.com>
2023-05-17 17:03:49 +0530

    final 1.0 usereducer

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -51,39 +51,114 @@ export default function CounterPage({ initialCount }) {
  */
 
 import { useReducer } from "react";
+import { produce } from "immer";
+
 import Button from "../components/Button";
 import Panel from "../components/Panel";
 
+const INCREMENT_COUNT = "increment";
+const DECREMENT_COUNT = "decrement";
+const SET_VALUE_TO_ADD = "change-value-to-add";
+const ADD_VALUE_TO_COUNT = "add_Value_to_count";
+
 const reducer = (state, action) => {
-  return {
-    ...state,
-    count: state.count + 1,
-  };
+  //when using immer produce
+  switch (action.type) {
+    case INCREMENT_COUNT:
+      state.count += 1;
+      return;
+    case DECREMENT_COUNT:
+      state.count -= 1;
+      return;
+    case SET_VALUE_TO_ADD:
+      state.valToAdd = action.payload;
+      return;
+    case ADD_VALUE_TO_COUNT:
+      state.count += state.valToAdd;
+      state.valToAdd = 0;
+      return;
+    default:
+      return state;
+  }
+
+  /*   switch (action.type) {
+    case INCREMENT_COUNT:
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+    case DECREMENT_COUNT:
+      return {
+        ...state,
+        count: state.count - 1,
+      };
+    case SET_VALUE_TO_ADD:
+      return {
+        ...state,
+        valToAdd: action.payload,
+      };
+    case ADD_VALUE_TO_COUNT:
+      return {
+        ...state,
+        count: state.count + state.valToAdd,
+        valToAdd: 0,
+      };
+    default:
+      return state;
+  } */
+  /*   if (action.type === INCREMENT_COUNT) {
+    return {
+      ...state,
+      count: state.count + 1,
+    };
+  }
+
+  if (action.type === DECREMENT_COUNT) {
+    return {
+      ...state,
+      count: state.count - 1,
+    };
+  }
+
+  if (action.type === SET_VALUE_TO_ADD) {
+    return {
+      ...state,
+      valToAdd: action.payload,
+    };
+  } */
 };
 export default function CounterPage({ initialCount }) {
   /*   const [count, setCount] = useState(initialCount);
   const [valToAdd, setValtoAdd] = useState(0); */
 
-  const [state, dispatch] = useReducer(reducer, {
+  const [state, dispatch] = useReducer(produce(reducer), {
     count: initialCount,
     valToAdd: 0,
   });
   const increment = () => {
-    dispatch(); //inc by 1 no need to pass any arg
+    dispatch({ type: INCREMENT_COUNT }); //inc by 1 no need to pass any arg
     //recat will find reducer func
   };
 
   const decrement = () => {
-    dispatch()
+    dispatch({ type: DECREMENT_COUNT });
   };
 
   const handleChange = (evt) => {
     const value = parseInt(evt.target.value) || 0;
+    dispatch({
+      type: SET_VALUE_TO_ADD,
+      payload: value,
+    });
   };
 
   const handleSubmit = (evt) => {
     evt.preventDefault();
+    dispatch({
+      type: ADD_VALUE_TO_COUNT,
+    });
   };
+  //console.log(state);
   return (
     <Panel className="m-3">
       <h1 className="text-lg">Count is {state.count}</h1>
@@ -95,7 +170,7 @@ export default function CounterPage({ initialCount }) {
       <form onSubmit={handleSubmit}>
         <label htmlFor="num">Add a lot!</label>
         <input
-          value={state.valToAdd || " "}
+          value={state.valToAdd || ""}
           type="number"
           id="num"
           className="p-1 m-3 bg-gray-50 border border-gray-300"
